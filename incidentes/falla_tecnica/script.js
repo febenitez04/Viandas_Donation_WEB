c29743c96aecd1bb084b43e20d064ed0bf4a87ea
febenitez04 <febenitez@frba.utn.edu.ar>
2024-08-19 13:23:48 -0300

    PinSearch Implementado

--- incidentes/falla_tecnica/script.js
+++ incidentes/falla_tecnica/script.js
@@ -1,34 +1,22 @@
-// Inicializar el mapa
-var map = L.map('map').setView([-34.6037, -58.3816], 13);
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    maxZoom: 18,
-}).addTo(map);
+// Nuestro código relacionado a Leaflet
+const map = L.map('map', {
+    center: [-34.6037425,-58.381669],
+    zoom: 17
+});
 
-// Crear los marcadores
-var marker1 = L.marker([-34.6025246, -58.3843585]).bindPopup('Primera heladera').openPopup().addTo(map);
-var marker2 = L.marker([-34.6024605, -58.3812774]).bindPopup('Segunda heladera').openPopup().addTo(map);
-var marker3 = L.marker([-34.6047476, -58.3795161]).bindPopup('Tercera heladera').openPopup().addTo(map);
+L.tileLayer(
+    'https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png',
+    { attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' }
+).addTo(map);
 
-// Agrupar los marcadores en un LayerGroup
-var markersLayer = new L.LayerGroup([marker1, marker2, marker3]).addTo(map);
+// Marcadores para las heladeras
+L.marker([-34.6025246,-58.3843585], { title: 'Primera heladera' }).bindPopup('Primera heladera').openPopup().addTo(map);
+L.marker([-34.6024605,-58.3812774], { title: 'Segunda heladera' }).bindPopup('Segunda heladera').openPopup().addTo(map);
+L.marker([-34.6047476,-58.3795161], { title: 'Tercera heladera' }).bindPopup('Tercera heladera').openPopup().addTo(map);
+L.marker([-34.598607968791924, -58.420107873800205], {title: 'Heladera Medrano' }).bindPopup('Heladera medrano').openPopup().addTo(map);
 
-// PinSearch component
-var searchBar = L.control.pinSearch({
-    position: 'topright',
-    placeholder: 'Buscar Heladera...',
-    buttonText: 'Search',
-    onSearch: function(query) {
-        console.log('Search query:', query);
-        // Handle the search query here
-    },
-    searchBarWidth: '200px',
-    searchBarHeight: '30px',
-    maxSearchResults: 3
-}).addTo(map);
-
-// Capturar clics en el mapa para posicionar un marcador
 var marker;
+
 map.on('click', function(e) {
     var lat = e.latlng.lat;
     var lng = e.latlng.lng;
@@ -40,3 +28,13 @@ map.on('click', function(e) {
     document.getElementById('latitude').value = lat;
     document.getElementById('longitude').value = lng;
 });
+
+// Integración de Leaflet.PinSearch
+L.control.pinSearch({
+    placeholder: 'Buscar heladera...',
+    onSearch: function(query) {
+        console.log('Buscando:', query);
+    },
+    focusOnMarker: true,
+    maxSearchResults: 3
+}).addTo(map);
